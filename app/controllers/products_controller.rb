f2d86f9bdd0898c1e2074027d25743cfefe1e674
allenbbgo <c8c886@gmail.com>
2019-01-31 18:06:02 +0800

    end ok

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,7 @@ class ProductsController < ApplicationController
     end
   
     def create
-      @product = Product.new(params[:product])
+      @product = Product.new(product_params)
       if @product.save
         redirect_to @product, notice: "Successfully created product."
       else
@@ -26,7 +26,7 @@ class ProductsController < ApplicationController
   
     def update
       @product = Product.find(params[:id])
-      if @product.update_attributes(params[:product])
+      if @product.update_attributes(product_params)
         redirect_to @product, notice: "Successfully created product."
       else
         render :edit
@@ -38,5 +38,16 @@ class ProductsController < ApplicationController
       @product.destroy
       redirect_to products_url, notice: "Successfully destroyed product."
     end
+
+
+
+
+    private
+    def product_params
+        params.require(:product).permit(:name,:price,:released_on,:publisher_id,:rating,:discontinued)
+      end
+
+
+
   end
   
